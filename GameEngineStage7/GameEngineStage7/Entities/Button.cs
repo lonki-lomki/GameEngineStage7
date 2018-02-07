08a7321a785b6f463d56fd5814ca13814652a4c9
Дмитрий Карташов <cit.utc@gmail.com>
2018-02-07 23:02:42 +0200

    Создан класс для реализации GUI-кнопки. Добавлены типы танков. План работ на ближайшее время.

---
+++ GameEngineStage7/GameEngineStage7/Entities/Button.cs
@@ -0,0 +1,10 @@
+﻿namespace GameEngineStage7.Entities
+{
+    /// <summary>
+    /// Класс создает GUI элемент в виде кнопки, которую можно нажимать
+    /// </summary>
+    public class Button
+    {
+
+    }
+}
